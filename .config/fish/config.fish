29202acaa133e5aec0a231987619b04416eb47fa
lmseidler <seidler118@gmail.com>
2025-09-17 13:57:43 +0200

    removed openrouter api key + removed fish_variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@ if status is-interactive
     set -Ux VISUAL /usr/bin/nvim
     set -Ux S_EDITOR /usr/bin/nvim
     set -Ux PATH $PATH $HOME/.local/bin
-    set -Ux OPENROUTER_API_KEY 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
 
     alias ls='ls --color=auto'
     alias grep='grep --color=auto'
